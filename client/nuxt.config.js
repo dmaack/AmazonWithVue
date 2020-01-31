d8344e1e189d5e88b2fced398ab201b123e8888f
dominique maack <dominiquemaack@gmail.com>
2020-01-31 13:30:26 -0700

    missing files

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -6,13 +6,16 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    script: [{ src: 'https://js.stripe.com/v3' }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/font-awesome/css/all.css' },
+      { rel: 'stylesheet', href: '/css/default.css' }
     ]
   },
   /*
